ec4965ef7f32ce7744e18f323498488300dd6d14
Corey <codeancrazy9@gmail.com>
2026-05-09 10:16:52 -0400

    Initial commit

---
+++ src/main/java/org/example/Dealership.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealership {
+    private String name;
+    private String address;
+    private String phoneNum;
+    private List<Vehicle> inventory;
+
+}
